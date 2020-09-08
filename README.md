7da18d1856c9ab9686b7e94b37a00d26af8c1e56
Azis Toktobaev <42677564+uhzees@users.noreply.github.com>
2020-09-08 09:11:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fibonacci-memo
+## Code implements the classic Fibonacci sequence WITH memoization
+
+## Author: Jan Pearce, Berea College
+
+## References:
+- This repo contains original work by Jan Pearce 
---
+++ main.cpp
@@ -0,0 +1,59 @@
+/* An implementation of the Fibonacci sequence WITH memoization
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fib(int x, vector<bool>& isknown, vector<int>& fibval){
+	/*  The program runs the recursive Fibonacci sequence with memo-ization
+		0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 134, ...
+		The original was devised by Fibonacci (1170 ad - 1250 ad), 
+		who used this to model the breeding of pairs of rabbits. 
+
+		pre: n is a small positive integoer
+		Post: This function uses the following dedinition
+		fib(x) = 0 for x <= 0
+		fib(1) = 1
+		fib(x) = fib(x - 1) + fib(x - 2)  for x >= 2 */
+
+	if (x >= 0) { 
+
+		if (isknown[x]) {
+			return fibval[x];
+		}
+		else{
+			cout << x << " ";
+			isknown[x] = true;
+
+			fibval[x] = (fib(x - 1, isknown, fibval) + fib(x - 2, isknown, fibval));
+			return fibval[x];
+		}
+	}
+	else { // x <= 0
+			return 0;
+	}
+}
+
+int main(){
+
+	char stop;
+	int n;
+
+	cout << "Enter a small positive integer: ";
+	cin >> n;
+
+	vector<bool> isknown(n+1, false); //initialize vector
+	vector<int> fibval(n+1);
+	isknown[0] = true;
+	isknown[1] = true;
+	fibval[0] = 0;
+	fibval[1] = 1;
+
+	cout << "Here are the function calls: ";
+	cout << "\nFibonacci of " << n << " is " << fib(n, isknown, fibval) << "." << endl;
+
+	cin >> stop; //to keep window open in certain cases
+
+    return 0;
+}
